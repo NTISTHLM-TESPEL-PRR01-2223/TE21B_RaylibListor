371ffe66152e7dec839a10b771268f96a5d4eaa4
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-10-26 14:25:52 +0200

    Initial commit

---
+++ TE21B_RaylibListor/Program.cs
@@ -0,0 +1,27 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Mickes fantastiska spel GOTY edition");
+Raylib.SetTargetFPS(60);
+
+Texture2D enemyTexture = Raylib.LoadTexture("nmy.png");
+Rectangle enemyRect = new Rectangle(
+  0, 0,
+  enemyTexture.width, enemyTexture.height
+  );
+
+while (!Raylib.WindowShouldClose())
+{
+  // LOGIK
+
+  // GRAFIK
+  Raylib.BeginDrawing();
+  Raylib.ClearBackground(Color.WHITE);
+
+  Raylib.DrawTexture(enemyTexture,
+    (int)enemyRect.x,
+    (int)enemyRect.y,
+    Color.WHITE
+  );
+
+  Raylib.EndDrawing();
+}
